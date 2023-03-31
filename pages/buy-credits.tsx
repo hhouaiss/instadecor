16f7c0f6df3ff7862c72774c5939a0428eeafc7a
hhouaiss <hassan.houaiss@gmail.com>
2023-04-01 00:58:43 +0200

    PK stripe to live

--- pages/buy-credits.tsx
+++ pages/buy-credits.tsx
@@ -44,7 +44,7 @@ export default function Pricing() {
           // @ts-ignore
           <stripe-pricing-table
             pricing-table-id="prctbl_1MobnNK4W9ejG97elHjeFCEq"
-            publishable-key="pk_test_51HGpOvK4W9ejG97eYSm02d1hgagCOAAcKQCtH7258w6fA8wxo2PRv2xs2wSUG2xkV2YLBc0h3HxKITTFeJGtWai500o6bqGFHF"
+            publishable-key="pk_live_51HGpOvK4W9ejG97eYSm02d1hgagCOAAcKQCtH7258w6fA8wxo2PRv2xs2wSUG2xkV2YLBc0h3HxKITTFeJGtWai500o6bqGFHF"
             client-reference-id={session.user.email}
             customer-email={session.user.email}
           />
