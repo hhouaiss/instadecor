cba97a71d61b1dbdb7be82d4701b73c392f29a32
hhouaiss <hassan.houaiss@gmail.com>
2023-04-04 00:47:17 +0200

    adding box icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import 'boxicons/css/boxicons.min.css'
 
 export default function Header({
   photo,
@@ -7,20 +8,23 @@ export default function Header({
 }: {
   photo?: string;
   email?: string;
-}) {
+}): JSX.Element {
   return (
     <header className="flex flex-col xs:flex-row justify-between items-center w-full mt-3 border-b pb-7 sm:px-4 px-2 border-gray-500 gap-2">
       <Link href="/dream" className="flex space-x-2">
-        <Image
+        {/* <Image
           alt="header text"
           src="/bed.svg"
           className="sm:w-10 sm:h-10 w-9 h-9"
           width={24}
           height={24}
-        />
+        /> */}
+        <div className="flex justify-center items-center">
+        <i className='bx bxs-magic-wand bx-sm'></i>
         <h1 className="sm:text-3xl text-xl font-bold ml-2 tracking-tight">
           Insta Decor
         </h1>
+        </div>
       </Link>
       {email ? (
         <div className="flex items-center space-x-4">
