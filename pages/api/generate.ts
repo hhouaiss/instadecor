7f0b89d70e0643993ac01aa75984185674d2c1ea
hhouaiss <hassan.houaiss@gmail.com>
2023-12-01 21:06:52 +0100

    Timeout setting

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -110,7 +110,7 @@ export default async function handler(
       } else if (jsonFinalResponse.status === "failed") {
         break;
       } else {
-        await new Promise((resolve) => setTimeout(resolve, 300));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
       }
     }
 
