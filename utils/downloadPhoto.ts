716205d6732d9d3c2ceb5b9b1a176d6b3e98c45e
hhouaiss <hassan.houaiss@gmail.com>
2023-12-09 23:16:35 +0100

    Refactor downloadPhoto function to use forceDownload function

--- utils/downloadPhoto.ts
+++ utils/downloadPhoto.ts
@@ -1,19 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Initialize Supabase
-const supabase = createClient('https://whhdhiptvqvzfzwchezb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoaGRoaXB0dnF2emZ6d2NoZXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIwMzc2NTAsImV4cCI6MjAxNzYxMzY1MH0.k2RAWh2WJk0flovuExkxVGc7AlaUYiA3HTdrPAzqCYw');
-
-async function uploadImageToSupabase(blob: Blob) {
-  const fileName = Date.now() + '.jpg'; // generate a file name based on the current timestamp
-  const { data, error } = await supabase.storage
-    .from('generated_img')
-    .upload(fileName, blob);
-
-  if (error) {
-    throw error;
-  }
-
-  return data?.path;
+function forceDownload(blobUrl: string, filename: string) {
+  let a: any = document.createElement("a");
+  a.download = filename;
+  a.href = blobUrl;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
 }
 
 export default function downloadPhoto(url: string, filename: string) {
@@ -25,10 +16,8 @@ export default function downloadPhoto(url: string, filename: string) {
   })
     .then((response) => response.blob())
     .then((blob) => {
-      // Upload the blob to Supabase
-      uploadImageToSupabase(blob).then((path) => {
-        console.log(`Image uploaded to Supabase at ${path}`);
-      });
+      let blobUrl = window.URL.createObjectURL(blob);
+      forceDownload(blobUrl, filename);
     })
     .catch((e) => console.error(e));
 }
